2a88485630cab8afc70b165a5a7acd952480df18
Matthias Untergassmair <mattu@stanford.edu>
2020-02-21 17:06:19 -0800

    better benchmark output

--- bench/benchmark.jl
+++ bench/benchmark.jl
@@ -8,40 +8,45 @@ function benchmark_interpolate(grid, data, rand_points)
     end
 end
 
-function benchmark(GridType::Type, n_dims::Int=6, n_points_per_dim::Int=15, n_rand_points::Int=100)
+function benchmark(GridType::Type, n_dims::Int; n_points_per_dim::Int=15, n_rand_points::Int=100, verbosity::Int=1)
 
     # construct grid
     grid = GridType((range(0, 1, length=n_points_per_dim) for _ in 1:n_dims)...)
     rand_points = rand(n_dims, n_rand_points)
     rand_data = rand(length(grid))
 
-    println("Benchmarking interpolation on $(label(grid))")
-    println("dimensionality:          $(n_dims)")
-    println("points per dimension:    $(n_points_per_dim)")
-    println("number of random points: $(n_rand_points)")
+    trial = @benchmark benchmark_interpolate($grid, $rand_data, $rand_points) samples=10000
 
-    t = @benchmark benchmark_interpolate($grid, $rand_data, $rand_points) samples=10000
-    trial_estimate = median(t)
-    println(trial_estimate)
+    if verbosity > 0
+        println("Benchmarking interpolation on $(label(grid))")
+        println("dimensionality:          $(n_dims)")
+        println("points per dimension:    $(n_points_per_dim)")
+        println("number of random points: $(n_rand_points)")
+        if verbosity > 1
+            show(stdout, MIME"text/plain"(), trial)  # make sure printing is verbose by using MIME
+        else
+            println("\n", trial)
+        end
+        println("\n--------------------------------------------------")
+    end
 
-    println("--------------------------------------------------")
-
-    return median(t)
+    return trial
 end
 
-for ndims in 1:10
+verbosity = 1
+for ndims in 1:6
 
     println("\n##################################################")
-    println("### SUMMARY (ndims = $(ndims))")
+    println("### Benchmark for ndims = $(ndims)")
     println("##################################################")
 
-    trial_estimate_rectangle = benchmark(RectangleGrid, ndims)
-    trial_estimate_simplex = benchmark(SimplexGrid, ndims)
+    trial_rectangle = benchmark(RectangleGrid, ndims; verbosity=verbosity)
+    trial_simplex = benchmark(SimplexGrid, ndims; verbosity=verbosity)
 
     # println(ratio(trial_estimate_rectangle, trial_estimate_simplex))
     println(
         "RectangleGrid vs SimplexGrid: ",
-        judge(trial_estimate_rectangle, trial_estimate_simplex)
+        judge(median(trial_rectangle), median(trial_simplex))
     )
     println("##################################################\n")
 

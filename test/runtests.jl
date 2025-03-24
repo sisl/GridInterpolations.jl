f12b512ce405990a50c110a93ae9675f6f39a99c
Anshrin Srivastava <Anshrin@gmail.com>
2025-03-24 18:50:27 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -321,7 +321,7 @@ end
 end
 
 @testset "NearestGrid" begin
-    grid = NearestGrid(1.0:3.0, [10.0, 12.0])  # 2D grid with 3×2 size
+    grid = NearestGrid([1.0, 3.0, 5.0], [2.0, 4.0])  # 2D grid with 3×2 size
     data = reshape([11.0, 12.0, 13.0, 21.0, 22.0, 23.0], 3, 2)  # 3 rows, 2 cols
     @test interpolate(grid, data, [1.1, 10.1]) == 11.0
     @test interpolate(grid, data, [2.9, 10.1]) == 13.0
